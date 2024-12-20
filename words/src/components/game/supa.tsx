18b978dd82e32d776cbfff3ca823d153c098cc43
hlakarki <kshap.karki@gmail.com>
2024-12-20 14:43:50 -0500

    lrn: mutation

--- words/src/components/game/supa.tsx
+++ words/src/components/game/supa.tsx
@@ -1,22 +1,50 @@
 "use client";
 import { trpc } from "@/utils/trpc";
+import {useState} from "react";
 
 export function Supabase() {
+  const [newName, setNewName] = useState<string>("");
   const { data, isLoading } = trpc.test.getTests.useQuery();
 
+  const utils = trpc.useUtils();
+
+  // mutation
+  const addTest = trpc.test.addTest.useMutation({
+    onSuccess: () => {
+      utils.test.getTests.invalidate().catch(console.error);
+      setNewName("");
+    }
+  })
+
   return (
-    <div className="flex flex-col gap-4">
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <div className="flex flex-col gap-2">
-          {data?.map((item) => (
-            <div key={item.id} className="text-white">
-              {item.name}
-            </div>
-          ))}
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-2">
+          <input
+              type="text"
+              value={newName}
+              onChange={(e) => setNewName(e.target.value)}
+              className="px-4 py-2 border rounded"
+              placeholder="Enter name"
+          />
+          <button
+              onClick={() => addTest.mutate({name: newName})}
+              className="px-4 py-2 bg-blue-500 text-white rounded"
+          >
+            Add
+          </button>
         </div>
-      )}
-    </div>
+
+        {isLoading ? (
+            <div>Loading...</div>
+        ) : (
+            <div className="flex flex-col gap-2">
+              {data?.map((item) => (
+                  <div key={item.id} className="text-white">
+                    {item.name}
+                  </div>
+              ))}
+            </div>
+        )}
+      </div>
   );
 }
